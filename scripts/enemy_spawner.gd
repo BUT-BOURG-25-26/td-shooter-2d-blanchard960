ca98609fac35b31dfe3c7f181ce5a009922add46
UNIV-LYON1\p2301053 <elyne.derocles@laposte.net>
2025-11-10 12:30:08 +0100

    fn session - spawner

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,20 +1,37 @@
 class_name EnemySpawner
 extends Node2D
 
-@export var enemy_scene : PackedScene
-@export var spawn_interval = 2
+@export var enemy_scenes : Array[PackedScene]
+@export var spawn_interval: Vector2
+@export var spawn_offset_y: float = 200
+@export var spawn_offset_x: float = 20
 var rng = RandomNumberGenerator.new()
 
 @onready var spawn_timer = $SpawnTimer
 
 func _ready() -> void:
-	spawn_timer.start(spawn_interval)
+	spawn_timer.start(1)
+	spawn_timer.timeout.connect(spawn_enemy)
 
-func _on_timer_timeout() -> void:
-	var enemy = enemy_scene.instantiate()
+func spawn_enemy() -> void :
+	if enemy_scenes.is_empty() :
+		return
+		
+	var camera = get_viewport().get_camera_2d()
+	var visible_rect = get_viewport().get_visible_rect()
 	
-	var rand_x = rng.randf_range(20, 580)
-	enemy.global_position.y = global_position.y
+	var x_min = camera.get_screen_center_position().x - (visible_rect.size.x/2) + spawn_offset_x
+	var x_max = camera.get_screen_center_position().x + (visible_rect.size.x/2) - spawn_offset_x
+	
+	var y_min = camera.get_screen_center_position().y - (visible_rect.size.y/2)
+	
+	var enemy = enemy_scenes.pick_random().instantiate()
+	
+	var rand_x = rng.randf_range(x_min, x_max)
+	
+	enemy.global_position.y = y_min - spawn_offset_y
 	enemy.global_position.x = rand_x
+	
 	get_tree().current_scene.add_child(enemy)
+	##spawn_timer.start(randf_range(spawn_interval.x,spawn_interval.y))
 	return 

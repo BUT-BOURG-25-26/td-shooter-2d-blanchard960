97097af1b1c2bf2624dfc929e7100a7ed32ad8bb
UNIV-LYON1\p2301053 <elyne.derocles@laposte.net>
2025-11-10 16:10:27 +0100

    correction life display

--- scripts/UI/life_holder_ui.gd
+++ scripts/UI/life_holder_ui.gd
@@ -1,9 +1,13 @@
 extends HBoxContainer
 
-##@export var life_icon_scene : 
+@export var life_icon_scene : PackedScene
 @onready var player: Player = $"../../Player"
 
 func _ready() -> void:
-	pass
-	##for i in range(player.health):
-		
+	for i in range(player.health):
+		var life_icon = life_icon_scene.instantiate()
+		add_child(life_icon)
+
+func _process(delta : float) -> void :
+	if player.health != get_child_count():
+		get_child((get_child_count()-1)).queue_free()

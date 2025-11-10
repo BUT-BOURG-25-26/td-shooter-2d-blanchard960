0c788d481d38b406b2278651c0f031b6e399c8c3
UNIV-LYON1\p2301053 <elyne.derocles@laposte.net>
2025-11-10 11:01:51 +0100

    début partie 4

---
+++ scripts/enemy.gd
@@ -0,0 +1,15 @@
+class_name Enemy
+extends CharacterBody2D
+
+@export var speedOutView: float = 500.0
+@export var speed: float = 100.0
+@export var pattern: int = 1
+
+func _physics_process(delta: float) -> void:
+	if (global_position.y < 100):
+		global_position.y += speedOutView * delta;
+	else :
+		match pattern :
+			1 :
+				global_position.x += speed * delta
+	return 

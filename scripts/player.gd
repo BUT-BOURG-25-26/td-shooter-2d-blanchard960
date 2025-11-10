9d8de25e7a747b3bbd67f150a3be397543e75000
UNIV-LYON1\p2301053 <elyne.derocles@laposte.net>
2025-11-10 09:51:49 +0100

    correction partie 1 & 2

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,22 @@
 class_name Player
 extends CharacterBody2D
 
-@export var move_speed:float = 200
+@export var move_speed:float = 500
+@export var move_threshold:float = 20
 
 var move_inputs: Vector2
 
-func _physics_process(delta: float) -> void:
-	read_move_inputs()
-	move_inputs *= move_speed * delta
 	
-	var velocity = Vector2(move_inputs.x,move_inputs.y)
+func _physics_process(delta: float) -> void:
+	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	velocity.x = x_input * move_speed
+	
+	if Input.is_action_pressed("click"):
+		var mouse_local = get_local_mouse_position()
+		if abs(mouse_local.x) > move_threshold :
+			velocity.x = move_speed if mouse_local.x > 0.0 else -move_speed
 		
-	if move_inputs != Vector2.ZERO:
-		global_position += velocity
-	return
-
-func read_move_inputs():
-	move_inputs.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	move_inputs.y = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
-	move_inputs = move_inputs.normalized()
+	
+	move_and_slide()
+	
 	return

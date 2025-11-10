06140b0e4bc96c5d3f67c8cc4404c8bfd12469ae
UNIV-LYON1\p2301053 <elyne.derocles@laposte.net>
2025-11-10 11:41:12 +0100

    spawner - non fonctionnel

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,2 +1,15 @@
 class_name EnemySpawner
 extends Node2D
+
+@export var enemy_scene : PackedScene
+@export var spawn_interval = 2
+
+@onready var spawn_timer = $SpawnTimer
+
+func _ready() -> void:
+	spawn_timer.start(spawn_interval)
+
+func _on_timer_timeout() -> void:
+	var enemy = enemy_scene.instantiate()
+	enemy.global_position = global_position
+	return 

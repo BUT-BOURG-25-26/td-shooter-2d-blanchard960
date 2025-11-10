c082c49b88e6cc615991692269417da44a922bd7
UNIV-LYON1\p2301053 <elyne.derocles@laposte.net>
2025-11-10 10:42:25 +0100

    correction partie 3

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,13 +1,9 @@
 class_name Bullet
 extends Area2D
 
-var isPlayerBullet = true
-@export var move_speed = 300
+@export var isAllied = true
+@export var speed = 500
+
 
 func _physics_process(delta: float) -> void:
-	var move = Vector2.ZERO
-	if isPlayerBullet :
-		move.y = -move_speed * delta
-	else :
-		move.y = move_speed * delta
-	global_position += move
+	global_position.y += -speed * delta if isAllied else speed * delta
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,21 +1,28 @@
 class_name Player
 extends CharacterBody2D
 
-@export var move_speed:float = 500
-@export var move_threshold:float = 20
+@export var speed:float = 500.0
+@export var move_threshold:float = 20.0
 @export var health: int = 5
 @export var max_health : int = 5
+@export var shootInterval:float = 0.75
+@export var bulletScene : PackedScene
+
+@onready var shootTimer : Timer = $ShootTimer
 
 
-	
+func _ready() -> void:
+	shootTimer.start(shootInterval)
+	shootTimer.timeout.connect(shoot)
+
 func _physics_process(delta: float) -> void:
 	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	velocity.x = x_input * move_speed
+	velocity.x = x_input * speed
 	
 	if Input.is_action_pressed("click"):
 		var mouse_local = get_local_mouse_position()
 		if abs(mouse_local.x) > move_threshold :
-			velocity.x = move_speed if mouse_local.x > 0.0 else -move_speed
+			velocity.x = speed if mouse_local.x > 0.0 else -speed
 		
 	
 	move_and_slide()
@@ -28,3 +35,11 @@ func take_damage(damage : int)-> void:
 		health = 0
 	else :
 		health -= damage
+	return 
+
+
+func shoot(): 
+	var bullet = bulletScene.instantiate()
+	bullet.global_position = global_position
+	get_tree().current_scene.add_child(bullet)
+	return
